6d2aee0518a5e5391e8133279249910d1d70198e
Georgi Georgiev <forgeletterai@gmail.com>
2026-05-19 17:19:28 +0300

    Avoid Google PKCE cookie callback failure

--- auth.ts
+++ auth.ts
@@ -177,6 +177,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      checks: ["none"],
     })
   )
 }
